b9a09bcd75d2d358863364bb6922755d4bd3ebc0
dumb-meh <omorfaruk549@gmail.com>
2025-08-16 15:30:00 +0600

    minor fixes

--- app/services/pronounication/pronounciation_route.py
+++ app/services/pronounication/pronounciation_route.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter,HTTPException,Body
-from .association import Association
-from .association_schema import association_response
+from .pronounciation import Pronounciation
+from .pronounciation_schema import pronounciation_response
 
 router= APIRouter()
-story= Association()
+story= Pronounciation()
 
-@router.post("/pronuncitation",response_model=association_response)
+@router.post("/pronuncitation",response_model=pronounciation_response)
 async def get_pronuncitation(request_data: str = Body(..., media_type="text/plain")):
     try:
         response=story.get_pronuncitation(request_data)
-        return association_response (response=response)
+        return pronounciation_response (response=response)
     
     except Exception as e:
         raise HTTPException (status_code=500, detail=str(e))
--- app/services/pronounication/pronounciation_schema.py
+++ app/services/pronounication/pronounciation_schema.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
 from typing import List, Optional, Any
 
-class pronuncitation_request(BaseModel):
+class pronounciation_response_request(BaseModel):
     words: str
     time_length:str
     language:str
 
-class pronuncitation_response(BaseModel):
+class pronounciation_response(BaseModel):
     create_sentence: List[Any]
